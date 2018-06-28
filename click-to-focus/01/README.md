fe50384ff4b34825e04aaf29d331c29d47343571
@micahstubbs <micah.stubbs@gmail.com>
2018-06-28 16:35:27 -0700

    add iteration that makes code nice to work with

---
+++ click-to-focus/01/README.md
@@ -0,0 +1,6 @@
+this iteration makes the code nice to work with
+
+---
+
+the original example from https://github.com/vasturiano/force-graph/blob/master/example/click-to-focus/index.html
+
---
+++ click-to-focus/01/vis.js
@@ -0,0 +1,18 @@
+fetch('miserables.json')
+  .then(res => res.json())
+  .then(data => {
+    const elem = document.getElementById('graph')
+
+    const Graph = ForceGraph()(elem)
+      .graphData(data)
+      .nodeLabel('id')
+      .nodeAutoColorBy('group')
+      .linkDirectionalParticles(2)
+      .linkDirectionalParticleWidth(1.4)
+      .onNodeHover(node => (elem.style.cursor = node ? 'pointer' : null))
+      .onNodeClick(node => {
+        // Center/zoom on node
+        Graph.centerAt(node.x, node.y, 1000)
+        Graph.zoom(8, 2000)
+      })
+  })
